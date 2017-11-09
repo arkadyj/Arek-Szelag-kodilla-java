777bc16dda48efd8230820bf153f8aedd15e73c5
arkadyj <szelaga@gmail.com>
2017-11-09 12:20:18 +0100

    poprawda do zad 6.3

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -6,14 +6,16 @@ public class OddNumbersExterminator {
 
     public List exterminate(ArrayList<Integer> numbers) {
 
-        for (Iterator<Integer> list = numbers.iterator(); list.hasNext(); ) {
-            Integer entry = list.next();
-            if(entry%2!=0) {
-                list.remove();
+        if (numbers instanceof List && numbers.size()!=0) {
+
+            for (Iterator<Integer> list = numbers.iterator(); list.hasNext(); ) {
+                Integer entry = list.next();
+                if (entry % 2 != 0) {
+                    list.remove();
+                }
             }
         }
         return numbers;
-
     }
 
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -3,6 +3,7 @@ package com.kodilla.testing.collection;
 import org.junit.*;
 import org.junit.Assert;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
@@ -27,7 +28,7 @@ private   OddNumbersExterminator oddNumbersExterminator;
         System.out.println("Testing empty list");
 
         //lepiej sprawdzić coś bardziej sensownego, czy nie jest nullem, i czy jest pusta, itp
-        Assert.assertTrue(oddNumbersExterminator.exterminate(lista) instanceof List);
+        Assert.assertTrue(oddNumbersExterminator.exterminate(lista).size()==0 && oddNumbersExterminator.exterminate(lista)!=null);
 
 
     }
@@ -36,17 +37,29 @@ private   OddNumbersExterminator oddNumbersExterminator;
     public void testOddNumbersExterminatorNormalList () {
 
         ArrayList<Integer> lista = new ArrayList<>();
+        ArrayList<Integer> listaEven = new ArrayList<>();
         Random rand = new Random();
         for (int i=0; i<100; i++) {
             //rand to nie jest dobry pomysł
-            lista.add(rand.nextInt(20)+1);
+
+            // zmodyfikowałem sposób generowania liczb do list. 50 liczb jest parzysta, 50 nieparzysta
+            if (i % 2 == 0) {
+                lista.add(((rand.nextInt(20) + 1) * 2) + 1);
+            }
+            else
+            {
+                lista.add((rand.nextInt(20) + 1) * 2);
+                listaEven.add(lista.get(i));
+            }
         }
+
         System.out.println("Testing filled list");
 
         //Po co sprawdzasz jakiego jest typu skoro to jest ustawione w definicji metody?
         //Skoro to test pozytywnego przyapdku to powinieneś sprawdzać czy zostały usunięte odpowiedznie liczby,
         //tzn stworzyć kolekcje z usuniętymi i porównać z tym co zwróci metoda
-        Assert.assertTrue(oddNumbersExterminator.exterminate(lista) instanceof List);
+        //Assert.assertTrue(oddNumbersExterminator.exterminate(lista) instanceof List);
+        Assert.assertEquals(listaEven,oddNumbersExterminator.exterminate(lista));
 
 
     }
@@ -56,5 +69,9 @@ private   OddNumbersExterminator oddNumbersExterminator;
     @Test
     public void testNull() throws Exception {
         oddNumbersExterminator.exterminate(null);
+
+        System.out.println("Testing null as list");
+        //Assert.assertEquals(null,oddNumbersExterminator.exterminate(null));
+        Assert.assertNull(oddNumbersExterminator.exterminate(null));
     }
 }
