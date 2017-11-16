18b11667956bfeaa1536a57e3c44e1cba5caf712
arkadyj <szelaga@gmail.com>
2017-11-16 21:29:11 +0100

    zad_7_5 poprawka v1

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -12,12 +12,28 @@ public class ArrayOperationsTestSuite {
         //Given
         int[] tab = new int[]{1, 4, 2, 3};
 
+
         //When
         Double result = ArrayOperations.getAverage(tab);
 
         //Then
         Assert.assertEquals(2.5, result, 0.0);
         Assert.assertEquals(4, tab.length);
+
+    }
+
+    @Test
+    public void testEmptyArrayGetAverage() {
+        //Given
+        int[] tab = new int[]{};
+
+
+        //When
+        Double result = ArrayOperations.getAverage(tab);
+
+        //Then
+        Assert.assertEquals(0, result, 0.0);
+        Assert.assertEquals(0, tab.length);
 
     }
 }
