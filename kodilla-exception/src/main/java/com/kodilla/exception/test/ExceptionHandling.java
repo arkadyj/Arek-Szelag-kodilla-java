6880f13ab9448e053e3141c00f9c730422259dcc
arkadyj <szelaga@gmail.com>
2017-11-21 20:58:30 +0100

    zad_8_3

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,18 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+
+
+    public static void exceptionHandling(SecondChallenge secondChallenge) {
+
+        try {
+            secondChallenge.probablyIWillThrowException(3.5, 7.5);
+        } catch (Exception e) {
+            System.out.println("Remember x must be smaller than 2 and grater than 1 and y must be different from 1.5");
+        } finally {
+            System.out.println("Write here a code you want to always run");
+        }
+    }
+
+
+}
