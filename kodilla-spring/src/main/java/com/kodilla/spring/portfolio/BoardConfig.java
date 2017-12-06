a628bcdaa6c17cc57258de1aa5970c5408f8249a
arkadyj <szelaga@gmail.com>
2017-12-06 14:55:27 +0100

    zad_10_3

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -0,0 +1,38 @@
+package com.kodilla.spring.portfolio;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BoardConfig {
+
+    @Autowired
+    Board board;
+
+    @Bean
+    public Board getBoard(){
+        return new Board(createTaskToDo(), inProgressList(), doneList());
+    }
+
+    @Bean
+    public TaskList createTaskToDo() {
+        return new TaskList();
+    }
+
+    @Bean
+    public TaskList inProgressList() {
+        return new TaskList();
+    }
+
+    @Bean
+    public TaskList doneList() {
+        return new TaskList();
+    }
+
+
+
+
+
+
+}
