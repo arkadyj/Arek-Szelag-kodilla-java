b5557e9318f3170b3d8fba731c22dff5a7c3be3c
arkadyj <szelaga@gmail.com>
2017-12-08 07:41:30 +0100

    zad_10_3

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -0,0 +1,22 @@
+package com.kodilla.spring.portfolio;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class BoardConfig {
+    //dsadada
+
+
+    @Bean
+    public Board createBoard() {
+        return new Board(createTask(), createTask(), createTask());
+    }
+
+    @Bean
+    @Scope("prototype")
+    public TaskList createTask() {
+        return new TaskList();
+    }
+}
