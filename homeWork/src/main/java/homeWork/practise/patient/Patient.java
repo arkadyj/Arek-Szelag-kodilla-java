a26dd1cfb235f7faa66321ce755fdb25ba4d072a
arkadyj <szelaga@gmail.com>
2017-12-14 19:32:11 +0100

    takie tam

---
+++ homeWork/src/main/java/homeWork/practise/patient/Patient.java
@@ -0,0 +1,20 @@
+package homeWork.practise.patient;
+
+public class Patient {
+
+    private String sName;
+    private String fName;
+
+    public Patient(String sName, String fName) {
+        this.sName = sName;
+        this.fName = fName;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+}
---
+++ homeWork/src/main/java/homeWork/practise/patient/PatientNFZ.java
@@ -0,0 +1,8 @@
+package homeWork.practise.patient;
+
+public class PatientNFZ extends Patient {
+
+    public PatientNFZ(String sName, String fName) {
+        super(sName, fName);
+    }
+}
