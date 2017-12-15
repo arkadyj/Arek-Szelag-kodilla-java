82c29751e7ef24003370d5117495edf140e87dfd
arkadyj <szelaga@gmail.com>
2017-12-15 14:49:43 +0100

    Swing

---
+++ homeWork/src/main/java/homeWork/practise/frame/Form1.java
@@ -0,0 +1,19 @@
+package homeWork.practise.frame;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Form1 {
+    private JPanel Calculator;
+    private JButton button1;
+
+    public Form1() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
