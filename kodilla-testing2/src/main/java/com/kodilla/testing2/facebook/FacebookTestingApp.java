db116e5c8e3efc926ed1882629d8f179f7ea9445
arkadyj <szelaga@gmail.com>
2018-03-07 11:57:49 +0100

    zad_23_3

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -0,0 +1,32 @@
+package com.kodilla.testing2.facebook;
+
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class FacebookTestingApp {
+
+    public static final String XPATH_SELECT_DAY = "//div[contains(@class, \"_58mq _5dbb\")]/div/span/span/select[1]";
+    public static final String XPATH_SELECT_MONTH = "//div[contains(@class, \"_58mq _5dbb\")]/div/span/span/select[2]";
+    public static final String XPATH_SELECT_YEAR = "//div[contains(@class, \"_58mq _5dbb\")]/div/span/span/select[3]";
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("https://facebook.com/");
+
+
+        WebElement selectComboDay = driver.findElement(By.xpath(XPATH_SELECT_DAY));
+        Select selectBoard = new Select(selectComboDay);
+        selectBoard.selectByIndex(1);
+
+        WebElement selectComboMonth = driver.findElement(By.xpath(XPATH_SELECT_MONTH));
+        Select selectBoardMonth = new Select(selectComboMonth);
+        selectBoardMonth.selectByIndex(1);
+
+        WebElement selectComboYear = driver.findElement(By.xpath(XPATH_SELECT_YEAR));
+        Select selectBoardYear = new Select(selectComboYear);
+        selectBoardYear.selectByIndex(19);
+    }
+}
